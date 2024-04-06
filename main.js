96787944a144485f496794c69d8825398a9bf6b3
WebMinzz <127040879+WebMinzz@users.noreply.github.com>
2024-04-06 23:30:05 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 
 
 
+//not ready ALERT
+function notReady() {
+    alert("This Page Not Ready!!");
+  }
+
+
+//Kalkulator
+var inputLabel = document.getElementById('inputLabel');
+
+function pushBtn(obj) {
+    var pushed = obj.innerHTML;
+
+    if (pushed == "=") {
+        inputLabel.innerHTML = eval(inputLabel.innerHTML);
+    }
+    else if (pushed == "AC") {
+        inputLabel.innerHTML = "0";
+    }
+    else {
+        if (inputLabel.innerHTML == "0") {
+            inputLabel.innerHTML = pushed;
+        }
+        else {
+            inputLabel.innerHTML += pushed;
+        }
+    }
+}
+
+    
+//Whatsap Lock
 function redirectToWhatsApp() {
     var password = prompt("Masukan Kunci:");
     if (password === "password123") {
