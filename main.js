5277f832e29f9b3666280ce2cc37bfbfd61bbcc0
WebMinzz <127040879+WebMinzz@users.noreply.github.com>
2024-06-01 23:42:51 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -7,35 +7,13 @@ function notReady() {
   }
 
 
-//Kalkulator
-var inputLabel = document.getElementById('inputLabel');
-
-function pushBtn(obj) {
-    var pushed = obj.innerHTML;
-
-    if (pushed == "=") {
-        inputLabel.innerHTML = eval(inputLabel.innerHTML);
-    }
-    else if (pushed == "AC") {
-        inputLabel.innerHTML = "0";
-    }
-    else {
-        if (inputLabel.innerHTML == "0") {
-            inputLabel.innerHTML = pushed;
-        }
-        else {
-            inputLabel.innerHTML += pushed;
-        }
-    }
-}
-
     
 //Whatsap Lock
 function redirectToWhatsApp() {
-    var password = prompt("Masukan Kunci:");
-    if (password === "password123") {
+    var password = prompt("Ketik 0:");
+    if (password === "0") {
         var phoneNumber = "6288294992685"; // Ganti dengan nomor tujuan Anda
-        var message = "P in Aja Bang"; // Pesan default
+        var message = "Hi!!"; // Pesan default
         var whatsappUrl = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
         window.location.href = whatsappUrl;
     } else {
