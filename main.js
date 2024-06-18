9a504cd693a01fd375dacfe183a19ead04deddbc
WebMinzz <127040879+WebMinzz@users.noreply.github.com>
2024-06-18 18:26:12 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,20 +3,20 @@
 
 //not ready ALERT
 function notReady() {
-    alert("This Page Not Ready!!");
+    alert("THIS PAGE IS NOT YET AVAILABLE");
   }
 
 
     
 //Whatsap Lock
 function redirectToWhatsApp() {
-    var password = prompt("Ketik 0:");
+    var password = prompt("KETIK 0 : ");
     if (password === "0") {
         var phoneNumber = "6288294992685"; // Ganti dengan nomor tujuan Anda
         var message = "Hi!!"; // Pesan default
         var whatsappUrl = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
         window.location.href = whatsappUrl;
     } else {
-        alert("Akses Ditolak!!");
+        alert("AKSES DITOLAK!!");
     }
 }
